cf3c85003d14a280594a8b6427c1ef0f4ecd4e50
Neil Wang <neilywang3@gmail.com>
2024-05-24 14:25:13 -0700

    Added snake ai

---
+++ Snake/model.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn   # neural network
+import torch.optim as optim
+import torch.nn.functional as F
+import os
+
+class Linear_QNet(nn.Module):
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x):
+        # tensor x is the input
+        # input goes through linear1, then through relu, then through linear2
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+    
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+        
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+    
+
+class QTrainer:
+
+    def __init__(self, model, lr, gamma):
+        self.lr = lr
+        self.gamma = gamma
+        self.model = model
+        self.optimizer = optim.Adam(model.parameters(), lr=self.lr) # optimization function
+        self.criterion = nn.MSELoss() # loss function
+    
+    def train_step(self, state, action, reward, next_state, game_over):
+        # convert to tensor for multiple value inputs
+        state = torch.tensor(state, dtype=torch.float)
+        next_state = torch.tensor(next_state, dtype=torch.float)
+        action = torch.tensor(action, dtype=torch.long)
+        reward = torch.tensor(reward, dtype=torch.float)
+        # (n, x)
+
+        if len(state.shape) == 1:
+            # (1, x)
+            # add a dimension
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            game_over = (game_over, )
+        
+        # 1: predicted Q values with current state
+        pred = self.model(state)
+
+        # 2: Q_new = R + y * max(next_predicted Q value) -> only do this if not done
+        target = pred.clone()
+        for idx in range(len(game_over)):
+            Q_new = reward[idx]
+            if not game_over[idx]:
+                Q_new = reward[idx] + self.gamma * torch.max(self.model(next_state[idx]))
+
+            target[idx][torch.argmax(action).item()] = Q_new
+        
+        # 3: backpropagation
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, pred)
+        loss.backward()
+
+        self.optimizer.step()
