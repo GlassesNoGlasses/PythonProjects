1a164e4dcc75e5c942c11d7a7f9c4be10aaff7d9
Neil Wang <neilywang3@gmail.com>
2024-07-27 20:21:16 -0700

    added firewall props

--- security/firewall.py
+++ security/firewall.py
@@ -1,15 +1,14 @@
 import socket
 import scapy.all as scapy
-import csv
-from ipaddress import ip_address, IPv4Address
+import pandas as pd
+from ipaddress import ip_address
 
 CSV_FIELDS = ['host', 'aliases', 'ips', 'inbound', 'outbound', 'protocol']
-
 
 class FirewallConfig():
     
     def __init__(self, csv_file: str, autosave: bool = True) -> None:
-        self.config = {}
+        self.config = pd.DataFrame(columns=CSV_FIELDS)
         self.default = {'inbound': True, 'outbound': True, 'protocol': None}
         self.csv_file = csv_file
         self.autosave = autosave
@@ -30,30 +29,24 @@ class FirewallConfig():
         ''' Filter out invalid ip addresses. '''
         return [ip for ip in ips if self.validIPAddress(ip)]
     
+
     def save_config(self) -> None:
         ''' Save the firewall configuration to a csv file. '''
 
-        with open(self.csv_file, 'w', newline='') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=CSV_FIELDS)
-            writer.writeheader()
+        self.config.to_csv(self.csv_file, columns=CSV_FIELDS, index=False)
 
-            for host, data in self.config.items():
-                writer.writerow({'host': host, 'aliases': data['aliases'], 'ips': data['ips'], 'inbound': data['inbound'], 'outbound': data['outbound'], 'protocol': data['protocol']})
     
-    def load_config(self) -> bool:
+    def load_config(self, csv: str) -> bool:
         ''' Load the firewall configuration from a csv file. '''
 
-        try:
-            with open(self.csv_file, 'r') as csvfile:
-                reader = csv.DictReader(csvfile)
-                for row in reader:
-                    self.config[row['host']] = {'aliases': row['aliases'], 'ips': row['ips'], 'inbound': row['inbound'], 'outbound': row['outbound'], 'protocol': row['protocol']}
-        except FileNotFoundError:
+        self.config = pd.read_csv(csv)
+
+        if (self.config.empty):
             return False
         
         return True
 
-    
+
     def update_config(self, host: str, aliases: list[str], ips: list[str], inbound: bool = True, 
                       outbound: bool = True, protocol: str | None = None) -> bool:
         ''' Update the configuration. '''
@@ -63,40 +56,44 @@ class FirewallConfig():
         if not filtered_ips:
             return False
         
-        self.config[host] = {'aliases': aliases, 'ips': filtered_ips, 'inbound': inbound, 'outbound': outbound, 'protocol': protocol}
-
+        pd.concat(self.config, pd.DataFrame([host, aliases, filtered_ips, inbound, outbound, protocol], columns=CSV_FIELDS))
+        
         if self.autosave:
             self.save_config()
         
         return True
     
-    def delete_config(self, host: str) -> bool:
+    def delete_config(self, host: str) -> None:
         ''' Delete a configuration. '''
-        if host in self.config:
-            del self.config[host]
-            return True
-        return False
+
+        self.config = self.config.drop(host)
+
+        if self.autosave:
+            self.save_config()
+    
+    def show_config(self, count: None | int = None) -> dict:
+        ''' Show the configuration. '''
+
+        print(self.config.head(count)) if count else print(self.config)
 
 
 class Firewall():
 
-    def __init__(self, ip_addr = None, auto_save: bool = True, csv_file: str = "./firewall.csv") -> None:
+    def __init__(self, ip_addr: str = "0.0.0.0", auto_save: bool = True, csv_file: str = "./firewall.csv") -> None:
 
         self.config = FirewallConfig(autosave=auto_save, csv_file=csv_file)
-        self.ip_addr = ip_addr if ip_addr else self.get_ip()
+        self.ip_addr = ip_addr
         self.csv_file = csv_file
     
-    def get_ip():
-        ''' Get's the ip address of the host. '''
-        return socket.gethostbyname(socket.gethostname())
-        
-    def get_ip_from_host(host: str):
+
+    def get_ip_from_host(self, host: str):
         ''' Return (hostname, aliases, ipaddrlist) from host name. Supports ipv4 only.'''
         try:
             ips = socket.gethostbyname_ex(host)
         except socket.gaierror:
             ips = None
         return ips
+
 
     def add_firewall(self, host: str, aliases: list[str], ips: list[str], inbound: bool = True, 
                      outbound: bool = True, protocol: str | None = None) -> bool:
@@ -123,6 +120,13 @@ class Firewall():
 def main():
     firewall = Firewall()
     print(firewall.ip_addr)
-    print(firewall.get_ip_from_host('www.google.com'))
-    print(firewall.get_ip_from_host('www.manga4life.com'))
+    google, google_aliases, google_ips = firewall.get_ip_from_host('www.google.com')
+    manga4life, manga4life_aliases, manga4life_ips = firewall.get_ip_from_host('www.manga4life.com')
+
+    firewall.add_firewall(google, google_aliases, google_ips)
+    firewall.add_firewall(manga4life, manga4life_aliases, manga4life_ips)
+
+    firewall.show_firewalls()
+
+main()
 
