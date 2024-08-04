2593c3753f2538f9f203b2640ed73ec3cc469042
Neil Wang <neilywang3@gmail.com>
2024-08-04 14:45:14 -0700

    added threading functionality

--- security/firewall-sim/sim.py
+++ security/firewall-sim/sim.py
@@ -7,7 +7,7 @@ from helper import generate_ip, validIPAddress
 
 class FirewallSim():
 
-    def __init__(self, host_ip: str = "10.10.10.10", port: int = 8080) -> None:
+    def __init__(self, host_ip: str = "127.0.0.1", port: int = 4444) -> None:
         ''' Initialize the firewall simulator. Optionally can specify a host/port to instantiate.'''
 
         self.host_ip = host_ip
@@ -15,12 +15,15 @@ class FirewallSim():
         self.senders = {}
 
         # start host socket
-        self.host = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-        self.host.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # reuse address if left in TIME_WAIT state
-        self.host.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1) # include IP headers
-        self.host.bind((host_ip, port)) # bind host socket to host_ip/port
+        try:
+            self.host = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+            self.host.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # reuse address if left in TIME_WAIT state
+            self.host.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1) # include IP headers
+        except PermissionError as e:
+            print(f"Error binding host socket: {e}")
+            return
+            
 
-    
     def instantiate_senders(self, num_senders: int = 3, sender_ips: list[str] | None = []) -> None:
         ''' Instantiate num_senders number of sender sockets. Optionally, pass in a list of sender IPs to use. 
             If no sender IPs are passed in, the simulator will generate random IPs. '''
@@ -39,10 +42,10 @@ class FirewallSim():
                         raise ValueError(f'Invalid IP address given: {ip}')
             
             for ip in sender_ips:
-                sender = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+                sender = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
                 sender.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-                sender.bind((ip, self.port))
                 self.senders[ip] = sender
+                sender.connect((ip, self.port))
         except ValueError as e:
             print(f"Error instantiating senders IPs: {e}")
             self.cleanup_senders()
@@ -71,7 +74,6 @@ class FirewallSim():
         print("Cleaning up...")
 
         try:
-            self.host.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
             self.host.shutdown(socket.SHUT_RDWR)
             self.host.close()
 
@@ -80,49 +82,71 @@ class FirewallSim():
             print(f"Error closing sockets: {e}")
 
         print("All sockets closed.")
+        exit()
 
-    def run(self) -> None:
-        ''' Start the simulation. '''
+    
+    def show_connections(self) -> None:
+        ''' Show the current senders and host information. '''
 
+        print("Host IP: ", self.host_ip)
+        print("Host Port: ", self.port)
+        print("Senders: ", self.senders)
+
+
+    def run(self, packet_count: int = -1, num_senders: int = 3, sender_ips: list[str] | None = []) -> None:
+        ''' Start the simulation. Optionally, specify the number of packets to send. '''
+
+        packet_count = packet_count if packet_count > 0 else float('inf')
+        count = 0
+
+        # set up host to listen for packets
+        self.host.bind((self.host_ip, self.port)) # bind host socket to host_ip/port
+        self.instantiate_senders(num_senders, sender_ips)
+        
         print("[START]: Starting simulation...")
-
-        self.host.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 
         try:
             # start senders
             for sender in self.senders.keys():
                 thread = threading.Thread(target=start_sender, args=(self.senders[sender], sender, self.port, self.host_ip))
-                thread.setDaemon(True)
+                thread.daemon = True
                 thread.start()
  
             # start listening for packets
-            while True:
+            while count < packet_count:
                 packet, addr = self.host.recvfrom(65565)
-                print(f"Received packet from {addr}: {packet}")
-        except KeyboardInterrupt:
-            print("\n[END]: Simulation ended...")
-            self.shutdown()
-            return
+                logging.info(f"Received packet from {addr}: {packet}")
+                count += 1
+    
+        except Exception as e:
+            print(f"Error receiving packet: {e}")
+            exit()
+        
+        print("[END]: Simulation ended.")
+        self.shutdown()
+        return
 
 
-def send_packet(sender: socket.socket, port: int, dest_ip: str, payload) -> None:
+def send_packet(sender: socket.socket, source_ip: str, port: int, dest_ip: str, payload) -> None:
     ''' Send a packet with payload payload from the sender to dest_ip. dest_ip defaults to the host IP. '''
 
-    if not validIPAddress(dest_ip):
-        print("Invalid destination IP address.")
+    if not validIPAddress(dest_ip) or not validIPAddress(source_ip):
+        print("Invalid source/dest IP addresses.")
+
+    logging.info(f"Sending packet from {source_ip} to {dest_ip}...")
 
     try:
-        sender.sendto(payload, (dest_ip, port))
+        sender.send(payload, 65565)
     except InterruptedError as e:
         print(f"Packet sending interrupted: {e}")
         return
     except OSError as e:
-        print(f"Error sending packet: {e}")
+        print(f"[Error] Sending packet: {e}")
         return
     
 
-def start_sender(sender: socket.socket, source_ip: str, port: int, dest_ip: str, interval: int = 1000) -> None:
-    ''' Start the sender to send packets to dest_ip. Specify the interval in milliseconds; defaults to 1000ms. '''
+def start_sender(sender: socket.socket, source_ip: str, port: int, dest_ip: str, interval: int = 1) -> None:
+    ''' Start the sender to send packets to dest_ip. Specify the interval in seconds; defaults to 1s. '''
 
     if not validIPAddress(source_ip) or not validIPAddress(dest_ip):
         print(f"Invalid source/dest IP address. Source: {source_ip}, Dest: {dest_ip}.")
@@ -132,17 +156,28 @@ def start_sender(sender: socket.socket, source_ip: str, port: int, dest_ip: str,
     payload = b"Hello from " + source_ip.encode()
 
     try:
-        send_packet(sender, port, dest_ip, payload)
+        # send packet every interval seconds
+        timer = threading.Timer(interval=interval, function=send_packet, args=(sender, source_ip, port, dest_ip, payload))
+        timer.daemon = True
+        timer.start()
 
     except KeyboardInterrupt:
         print(f"\n Exiting Client {source_ip}...")
+        timer.cancel()
         return
     except Exception as e:
-        print(f"Error sending packet: {e}")
+        print(f"[Error] Starting Thread Packet: {e}")
         return
 
 
 
 if __name__ == '__main__':
-    logger = logging.basicConfig(filemode='w', filename='firewall.log', level=logging.INFO)
+    logging.basicConfig(filemode='w', filename='firewall.log', level=logging.INFO)
+
+    sim = FirewallSim()
+
+    logging.info("Starting firewall simulation...")
+    sim.run(packet_count=10)
+    logging.info("Ending firewall simulation")
+
     pass
